747f0799c451f81a78fe5a6b19760683a10895ff
Radek Matěj <radek@nikdo.cz>
2016-04-06 23:57:11 +0200

    Add break and done sound effects

--- src/config.js
+++ src/config.js
@@ -9,3 +9,13 @@ export const IDLE = 0
 export const WORK = 1
 export const BREAK = 2
 export const DONE = 3
+
+export const DONE_SOUND = 'bell_ring'
+export const BREAK_SOUND = 'door_bell'
+
+window.ion.sound({
+	sounds: [{ name: DONE_SOUND }, { name: BREAK_SOUND }],
+	path: 'sounds/',
+	volume: 0.6,
+	preload: true
+})
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import DocumentTitle from 'react-document-title'
 import { start, stop } from '../actions'
-import { states } from '../config'
+import { states, BREAK, DONE, BREAK_SOUND, DONE_SOUND } from '../config'
 import Counter from '../components/Counter'
 import Controls from '../components/Controls'
 
@@ -16,11 +16,21 @@ class App extends Component {
 		this.props.dispatch(stop())
 	}
 
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.status != this.props.status) {
+			if (nextProps.status == BREAK)
+				window.ion.sound.play(BREAK_SOUND)
+			else if (nextProps.status == DONE)
+				window.ion.sound.play(DONE_SOUND)
+		}
+	}
+
 	render() {
+		const status = states[this.props.status]
 		return (
-			<DocumentTitle title={getTitle(this.props.status.name, this.props.remainingTime)}>
+			<DocumentTitle title={getTitle(status.name, this.props.remainingTime)}>
 				<div>
-					<Counter status={this.props.status} remainingTime={this.props.remainingTime} />
+					<Counter status={status} remainingTime={this.props.remainingTime} />
 					<Controls
 						paused={this.props.paused}
 						start={() => this.props.dispatch(start())}
@@ -33,7 +43,7 @@ class App extends Component {
 
 const mapStateToProps = (state) => {
 	return {
-		status: states[state.status],
+		status: state.status,
 		remainingTime: Math.floor(state.seconds/60) + ':' + ('0' + state.seconds%60).slice(-2),
 		paused: state.paused
 	}
